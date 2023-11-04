2a34d777acef1d7cad7367bee5d42c1d108def5a
AK-47 <ans_khan@hotmail.com>
2023-11-04 17:14:02 +0300

    New project

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -22,7 +22,7 @@ function createPassword(){
         password += allChars[Math.floor(Math.random() * allChars.length)];
 
     }
-    passwordBox.value = password;
+    passwordBox.value = password;   
 }
 
 function copyPassword() {
